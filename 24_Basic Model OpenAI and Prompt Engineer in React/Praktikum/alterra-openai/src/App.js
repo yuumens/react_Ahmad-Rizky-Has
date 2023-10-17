0b020ba62bb9bad74900455109f517e46abefc96
YumanYummy <ahmadrizki0209@gmail.com>
2023-10-17 21:44:00 +0700

    main : add Praktikum and Screenshot folder Content

---
+++ 24_Basic Model OpenAI and Prompt Engineer in React/Praktikum/alterra-openai/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Homes from "./components/home/homes";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Homes />
+    </div>
+  );
+};
+
+export default App;
